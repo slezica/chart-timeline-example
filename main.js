27b6c065beb0d36affbf2b0bafea7a6e7947837b
Santiago Lezica <slezica89@gmail.com>
2015-09-14 20:55:07 -0300

    V...4?

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
+var colors = [
+  'rgb(220, 20, 20)',
+  'rgb(20, 20, 220)'
+]
+
 $(document).ready(function() {
   var items = generateItems()
   var data   = toChartData(items)
-
   var selectedItem = null
 
   var canvas = $('#chart canvas')
   var line = new Chart(canvas[0].getContext('2d')).Line(data, {
     scaleUse2Y: true,
     bezierCurve: false,
-
-    customTooltips: function(tooltip) {
-      if (! tooltip || ! selectedItem) return
-      tooltip.canvas = canvas
-      showTooltip(tooltip, selectedItem)
-    }
+    customTooltips: function() {}
   })
 
   canvas.mousemove(function(e) {
@@ -23,7 +22,7 @@ $(document).ready(function() {
 
     if (! selectedItem || id !== selectedItem.id) {
       selectedItem = items[id]
-      onInspectItem(selectedItem)
+      onInspectItem(selectedItem, items)
     }
   })
 
@@ -33,20 +32,37 @@ $(document).ready(function() {
 })
 
 
-function onInspectItem(item) {
-  $ul = $('#events ul')
+function onInspectItem(item, items) {
+  $sidebar = $('#sidebar')
+  $events = $('#sidebar').find('.events')
+  $values = $sidebar.find('.values')
+
   var duration = 300 // fade in + fade out
 
-  $ul.fadeOut(duration / 2, function after() {
-    $ul.empty()
+  $sidebar.fadeOut(duration / 2, function after() {
+    $events.empty()
 
     if (item) {
       _.each(item.events, function(event) {
-        $('#events ul').append('<li>' + event + '</li>')
+        $events.append('<li class="event"><span class="plus">+</span><img src="pic.png" class="avatar"><span class="name">' + event + '</span></li>')
+      })
+
+      _.times(2, function(i) {
+        $line = $values.find('.line-' + (i + 1))
+
+        var value = item.values[i].toString()
+        $line.find('.value').text(value)
+
+        if (items[item.id - 1]) {
+          var delta = item.values[i] - items[item.id - 1].values[i]
+          $line.find('.delta').text('(' + delta + ')')
+        }
+
+        $line.find('.color').css({ backgroundColor: colors[i] })
       })
     }
 
-    $ul.fadeIn(duration / 2)
+    $sidebar.fadeIn(duration / 2)
   })
 }
 
@@ -70,7 +86,7 @@ function randomItem(id) {
   return {
     id    : id,
     values: _.times(2, function(i) { return randInt(50) * (i + 1) }),
-    events: _.times(randInt(3) + 1, function(i) { return 'Event ' + id + ': ' + (i + 1) })
+    events: _.times(randInt(2) + 1, function(i) { return 'Person ' + id + ': ' + (i + 1) })
   }
 }
 
@@ -87,58 +103,26 @@ function toChartData(items) {
       {
         label: "Dataset 1" ,
         fillColor: "transparent",
-        strokeColor: "rgba(220,20,20,1)",
-        pointColor: "rgba(220,20,20,1)",
+        strokeColor: colors[0],
+        pointColor: colors[0],
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)",
+        pointHighlightStroke: "#000",
         data: _.map(items, function(item) {
           return new ChartPoint(item.id, item.values[0])
         })
       }, {
         label: "Dataset 2",
         fillColor: "transparent",
-        strokeColor: "rgba(20,20,110,1)",
-        pointColor: "rgba(20,20,110,1)",
+        strokeColor: colors[1],
+        pointColor: colors[1],
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)",
+        pointHighlightStroke: "#000",
         data: _.map(items, function(item) {
           return new ChartPoint(item.id, item.values[1])
         })
       }
     ]
   }
-}
-
-//
-// console.log(
-//   $canvas.offset().left,
-//   $canvas.offset().top,
-//   tooltip.x + 'px',
-//   tooltip.y + 'px'
-// )
-
-function showTooltip(tooltip, item) {
-  $tooltip = $('#chart .tooltip')
-
-  $tooltip.hide()
-  if (! tooltip) return
-
-  $tooltip.css({
-    left: tooltip.canvas.offset().left + tooltip.x + 'px',
-    top: tooltip.canvas.offset().top + tooltip.y + 'px',
-  })
-
-  _.times(2, function(i) {
-    $line = $tooltip.find('.line-' + (i + 1))
-
-    $line.find('.color').css({ backgroundColor: tooltip.legendColors[i].fill })
-    $line.find('.value').text(item.values[i])
-    console.log($line.find('.value')[0]);
-  })
-
-  $tooltip.find('.events').text(item.events.length + ' events')
-
-  $tooltip.show()
 }
